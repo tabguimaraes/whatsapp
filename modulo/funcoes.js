7a29749cc0acf593c511ee576bc17ccc1c0d0b29
Tiago Guimarães <tab.guimaraes@gmail.com>
2025-09-24 10:47:43 -0300

    add listarDadosDeContato. Testes OK

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -29,15 +29,15 @@ function listarDadosDaConta(number) {
     message.dados = [];
     let usuario = dados.contatos["whats-users"].find((item) => item.number === number);
 
-    message.dados.push(
-      { nome: usuario.account },
-      { nickname: usuario.nickname },
-      { "profile-image": usuario["profile-image"] },
-      { number: usuario.number },
-      { background: usuario.background },
-      { "created-since-start": usuario["created-since"].start },
-      { "created-since-end": usuario["created-since"].end }
-    );
+    message.dados.push({
+      nome: usuario.account,
+      nickname: usuario.nickname,
+      "profile-image": usuario["profile-image"],
+      number: usuario.number,
+      background: usuario.background,
+      "created-since-start": usuario["created-since"].start,
+      "created-since-end": usuario["created-since"].end,
+    });
 
     return message;
   } catch (error) {
@@ -45,9 +45,23 @@ function listarDadosDaConta(number) {
   }
 }
 
-console.log(listarDadosDaConta("1194457796"));
+function listarDadosDeContato(number) {
+  try {
+    message.dados = [];
+    let usuario = dados.contatos["whats-users"].find((item) => item.number === number);
+
+    usuario.contacts.forEach((item) => {
+      message.dados.push({ name: item.name, description: item.description, image: item.image });
+    });
+
+    return message;
+  } catch (error) {
+    return MESSAGE_ERROR;
+  }
+}
 
 module.exports = {
   listarTodosUsuarios,
   listarDadosDaConta,
+  listarDadosDeContato,
 };
