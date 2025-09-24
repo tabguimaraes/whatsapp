01e382a857fdcf6dd3a4cd8c6bc37e1a3f1bc134
Tiago Guimarães <tab.guimaraes@gmail.com>
2025-09-24 15:35:17 -0300

    ajustes gerais de nome de variavel e organização de funções

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -6,14 +6,20 @@ const MESSAGE_ERROR = {
   development: "Tiago Guimarães",
 };
 
-const message = { status: true, status_code: 200, development: "Tiago Guimarães", dados: [] };
+const message = { status: true, status_code: 200, development: "Tiago Guimarães", data: [] };
 
 let usuario;
+
+// Função para selecionar o usuário pelo número do telefone e salvar os dados dentro da variável 'usuário'
+function selecionarUsuarioPeloTelefone(number) {
+  usuario = dataJSON.contatos["whats-users"].find((item) => item.number === number);
+  return usuario;
+}
 
 function listarTodosUsuarios() {
   try {
     dataJSON.contatos["whats-users"].forEach((item) => {
-      message.dados.push(item);
+      message.data.push(item);
     });
     return message;
   } catch (error) {
@@ -23,8 +29,8 @@ function listarTodosUsuarios() {
 
 function listarDadosDaConta(number) {
   try {
-    selecionarUsuario(number);
-    message.dados.push({
+    selecionarUsuarioPeloTelefone(number);
+    message.data.push({
       nome: usuario.account,
       nickname: usuario.nickname,
       "profile-image": usuario["profile-image"],
@@ -42,11 +48,11 @@ function listarDadosDaConta(number) {
 
 function listarDadosDeContato(number) {
   try {
-    message.dados = [];
+    message.data = [];
     usuario = dataJSON.contatos["whats-users"].find((item) => item.number === number);
 
     usuario.contacts.forEach((item) => {
-      message.dados.push({ name: item.name, description: item.description, image: item.image });
+      message.data.push({ name: item.name, description: item.description, image: item.image });
     });
 
     return message;
@@ -57,20 +63,15 @@ function listarDadosDeContato(number) {
 
 function listarTodasMensagens(number) {
   try {
-    selecionarUsuario(number);
+    selecionarUsuarioPeloTelefone(number);
     usuario.contacts.forEach((item) => {
-      message.dados.push({ contato: item.name }, { mensagens: item.messages });
+      message.data.push({ contato: item.name }, { mensagens: item.messages });
     });
 
     return message;
   } catch (error) {
     return MESSAGE_ERROR;
   }
-}
-
-function selecionarUsuario(number) {
-  usuario = dataJSON.contatos["whats-users"].find((item) => item.number === number);
-  return usuario;
 }
 
 module.exports = {
